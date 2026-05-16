e845ff9928e61496fa5b559beeaa5d899f9fe4ba
Mahmoud921 <hoda1892003@gmail.com>
2026-05-17 00:55:46 +0300

    Add Api Responces Message

---
+++ Architecture.Application/Common/Responses/ApiResponse.cs
@@ -0,0 +1,43 @@
+﻿namespace Architecture.Application.Common.Responses
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+
+        public string Message { get; set; } = string.Empty;
+
+        public T? Data { get; set; }
+
+        public IEnumerable<string>? Errors { get; set; }
+
+        public int StatusCode { get; set; }
+
+        public static ApiResponse<T> SuccessResponse(
+            T data,
+            string message = "Success",
+            int statusCode = 200)
+        {
+            return new ApiResponse<T>
+            {
+                Success = true,
+                Message = message,
+                Data = data,
+                StatusCode = statusCode
+            };
+        }
+
+        public static ApiResponse<T> FailureResponse(
+            string message,
+            IEnumerable<string>? errors = null,
+            int statusCode = 400)
+        {
+            return new ApiResponse<T>
+            {
+                Success = false,
+                Message = message,
+                Errors = errors,
+                StatusCode = statusCode
+            };
+        }
+    }
+}
