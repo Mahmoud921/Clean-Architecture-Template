6853bf531cf19883b8be43687453994c75a78c39
Mahmoud921 <hoda1892003@gmail.com>
2026-05-17 00:56:54 +0300

    Add Unitof work and register in Program.cs

---
+++ Architecture.Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using Architecture.Domain.Interfaces.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Architecture.Domain.Interfaces
+{
+    public interface IUnitOfWork:IDisposable
+    {
+        IRepository<T> Repository<T>() where T : class;
+
+        // Save everything in one transaction
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
