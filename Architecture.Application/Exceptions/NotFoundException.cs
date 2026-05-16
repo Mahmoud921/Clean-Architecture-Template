fd222f412f8ad98f8677cfa1d786c22bbcabcd26
Mahmoud921 <hoda1892003@gmail.com>
2026-05-17 00:54:32 +0300

    Add Custom Exception and Result Failure

---
+++ Architecture.Application/Exceptions/NotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Architecture.Application.Exceptions
+{
+    public class NotFoundException :BaseException
+    {
+        public NotFoundException(string message) :base(message ,(int)HttpStatusCode.NotFound)
+        {
+            
+        }
+    }
+}
