03230f56ac2349133f4106a3d8ebf65c643da9bb
Mahmoud921 <hoda1892003@gmail.com>
2026-05-17 00:55:26 +0300

    Add BaseEntitiy and Audit Classes

---
+++ Architecture.Domain/Primatives/AuditableEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Architecture.Domain.Primatives
+{
+    public class AuditableEntity
+    {
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public int CreatedBy { get; set; }
+        public int UpdatedBy { get; set; }
+
+    }
+}
